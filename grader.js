a185804f7b06a9eb61c13a9aca5a0f9411017d9f
Michael Agiorgousis <mikeaggro@gmail.com>
2013-07-17 01:58:42 +0000

    Updated grader.js to read from a url and conduct json checks

--- grader.js
+++ grader.js
@@ -3,8 +3,10 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "https://github.com/mikeaggro/bitstarter/blob/aecbd65545f3826de3c5731207991468ad575ca2/index.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -14,6 +16,14 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var getUrl = function(url, callback) {
+    rest.get(url).on('complete', function(result){
+	return callback(result.request.res.rawEncode);
+	})
+}; 
+
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -40,10 +50,17 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+    .option('-u, --url <url_location>', 'https://github.com/mikeaggro/bitstarter/blob/aecbd65545f3826de3c5731207991468ad575ca2/index.html')
+    .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
     .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    if(program.file) {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	}
+    else if (program.url) {
+        var checkJson = checkHtmlFile(getUrl(program.url), program.checks);
+	}
+
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 }
